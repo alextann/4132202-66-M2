45916aa5e243edd345f016bb7f341794f02b17ec
alextann <149346882+alextann@users.noreply.github.com>
2024-03-09 17:08:29 +0700

    LAB2

--- LAB2/html/app.js
+++ LAB2/html/app.js
@@ -1,25 +1,36 @@
-
-    var num = 10;
-    let name = "Am";
-    age = 19;
-    const year = 2004;
+var num = 10;
+    let name = "apec";
+    age = 20;
+    const year = 2003;
 
     console.log(year);
-    fruit = ["banana","apple","orange"];
-    plant = {fruit:"banana", car:"nissan",color:"orange"};
+
+
+    fruit = ["banana","apple","orage"];
+
+
+    console.log(fruit[1]);
+
+
+    plant = {fruit: "banana", car: "nissan", color:"orage"};
+
+    console.log(plant);
+
     dataJson = {
-    employee:[{name: "Am"} ,{ age: 19},{year:2004}],
+employee:[{ name:"apec" }, { age:20 }, { year: 2003}],
     };
-       
+
 console.log(dataJson.employee[0].name);
 
-let msg = name +year;
+    let msg = name + " " +year;
+    msg = `${name} ${year}`;
 
-console.log(msg);
+    console.log(msg);
 
-document.getElementById('title').innerHTML = msg;
+    document.getElementById('title').innerHTML = msg;
 
-$(function () {
-    $("#title")
-    .html(msg + "jQuery")
-})
+    $(function(){
+        $("#title")
+        .html(msg + " jQuery")
+        .addClass('red')
+    });
